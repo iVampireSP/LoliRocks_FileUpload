f978042c7c1555d66f482cee03bf1dca0a10e5b5
iVampireSP <ivampiresp@163.com>
2019-12-19 02:42:36 -0600

    优化上传时重命名文件的代码

--- done.php
+++ done.php
@@ -2,11 +2,11 @@
 	$ifuser = ($_COOKIE["user"]);
 	$guest = 'guest';
 	$zip = 'zip';
-	$name = ($ifuser .".". $zip);
+	$fn = $_FILES["file"]["name"];
 	if ($ifuser == $guest) {
             header("location: index.php");
           }else {
-	move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" . $name);
+	move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" . $ifuser.'-'.$fn);
 	header ("location: is_ok.php");
 	}
 ?>
