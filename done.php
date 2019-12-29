ee0dd8e1321313a2a3da4c340e6a6d1613538a03
iVampireSP <ivampiresp@163.com>
2019-12-29 20:22:09 +0800

    增加文件更名

--- done.php
+++ done.php
@@ -6,8 +6,8 @@
 	if ($ifuser == $guest) {
             header("location: index.php");
           }else {
-	move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" . $ifuser.'-'.$fn);
+	move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" . $ifuser.'-'.$fn.'.'.'rename');
 	//header ("location: is_ok.php");
-	header ("location: is_ok.php?filename=$ifuser-$fn");
+	header ("location: is_ok.php?filename=$ifuser-$fn.rename");
 	}
 ?>
